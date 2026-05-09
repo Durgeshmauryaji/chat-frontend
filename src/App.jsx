813c8432687e610ec2b0922a12ae5057ffba77e8
Durgesh Kumar <161188642+Durgeshmauryaji@users.noreply.github.com>
2026-05-09 10:58:32 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,56 +1,117 @@
 import React, { useState } from "react";
 import io from "socket.io-client";
 import { Chat } from "./Chat";
+
 import music from "./mixkit-tile-game-reveal-960.wav";
 
-const socket = io.connect("https://chat-backend-jqtd.onrender.com");
+// SOCKET CONNECTION
+
+const socket = io.connect(
+"https://chat-backend-jqtd.onrender.com"
+);
+// const socket = io.connect(
+// "http://localhost:1000"
+// );
+
 
 const App = () => {
-  const [username, setUsername] = useState("");
-  const [room, setRoom] = useState("");
-  const [showChat, setShowChat] = useState(false);
-  const notification = new Audio(music);
-  const joinChat = () => {
-    if (username !== "" && room !== "") {
-      socket.emit("join_room", room);
-      setShowChat(true);
-      notification.play();
-    }
-  };
 
-  return (
-    <>
-      {!showChat ? (
-       
-          
+// STATES
 
-          <div className="main">
-            {/* <img src="./src/ui-chat.png" className='img'/> */}
+const [username, setUsername] = useState("");
 
-            <div className="join_room ">
-              <h1>Join Chat</h1>
-              <p>Chat No. should be same both side.</p>
+const [room, setRoom] = useState("");
+
+const [showChat, setShowChat] = useState(false);
+
+const notification = new Audio(music);
+
+// JOIN CHAT
+
+const joinChat = () => {
+
+if (username !== "" && room !== "") {
+
+  // SEND ROOM + USERNAME
+
+  socket.emit("join_room", {
+    room,
+    username,
+  });
+
+  setShowChat(true);
+
+  notification.play();
+}
 
 
-              <input
-                type="text"
-                placeholder="Enter your name"
-                onChange={(e) => setUsername(e.target.value)}
-              />
-              <input
-                type="text"
-                placeholder="Enter chat no.to connect as 5,8 or any"
-                onChange={(e) => setRoom(e.target.value)}
-              />
-              <button onClick={joinChat}>Join</button>
-            </div>
-          </div>
-       
-      ) : (
-        <Chat socket={socket} username={username} room={room} />
-      )}
-    </>
-  );
+};
+
+return (
+
+
+<>
+
+  {!showChat ? (
+
+    <div className="main">
+
+      <div className="join_room">
+
+        <h1>Join Chat</h1>
+
+        <p>
+          Enter same room number to connect
+        </p>
+
+        {/* USERNAME */}
+
+        <input
+          type="text"
+
+          placeholder="Enter your name"
+
+          onChange={(e) =>
+            setUsername(e.target.value)
+          }
+        />
+
+        {/* ROOM */}
+
+        <input
+          type="text"
+
+          placeholder="Enter room number"
+
+          onChange={(e) =>
+            setRoom(e.target.value)
+          }
+        />
+
+        {/* BUTTON */}
+
+        <button onClick={joinChat}>
+          Join Chat
+        </button>
+
+      </div>
+
+    </div>
+
+  ) : (
+
+    <Chat
+      socket={socket}
+      username={username}
+      room={room}
+    />
+
+  )}
+
+</>
+
+
+);
 };
 
 export default App;
